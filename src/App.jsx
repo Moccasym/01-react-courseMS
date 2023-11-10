97b08ea6cee854d5d87ab8ffe4e25ec684443d58
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-11-10 14:13:10 +0100

    cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './components/Header/Header.jsx';
 
-
 import CoreConcepts from './components/CoreConcepts.jsx';
 import Examples from './components/Examples.jsx';
 
 function App() {
-
 
   return (
     <>
